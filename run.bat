41fb409b5c70bc28f903e4af8ed09c8f413820a4
Kalyan_Mundra <43372839+AutomationTestingDemo@users.noreply.github.com>
2020-08-24 13:03:20 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-pytest -s -v -m "sanity and regression" testCases/ --browser chrome --html=./Reports/jenkinsExecution.html
+pytest -s -v -m "regression" testCases/ --browser chrome --html=./Reports/jenkinsRegressionExecution.html
+pytest -s -v -m "sanity" testCases/ --browser chrome --html=./Reports/jenkinsSanityExecution.html
